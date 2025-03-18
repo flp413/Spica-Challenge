503fb2cdb562e6fe708331971282db9d652ffd44
flp413 <sara.mihalic02@gmail.com>
2025-03-18 23:01:53 +0100

    feat(routes): update user and absence routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'settings', pathMatch: 'full' },
-  { path: 'user-search', loadComponent: () => import('./features/user/user-search/user-search.component').then(m => m.UserSearchComponent) },
-  { path: 'user-grid', loadComponent: () => import('./features/user/user-grid/user-grid.component').then(m => m.UserGridComponent) },
-  { path: 'absence-grid', loadComponent: () => import('./features/absences/absence-grid/absence-grid.component').then(m => m.AbsenceGridComponent) },
+  { path: 'users', loadComponent: () => import('./features/user/user-grid/user-grid.component').then(m => m.UserGridComponent) },
+  { path: 'absence', loadComponent: () => import('./features/absences/absence-grid/absence-grid.component').then(m => m.AbsenceGridComponent) },
   { path: 'settings', loadComponent: () => import('./features/settings/settings.component').then(m => m.SettingsComponent) },
   { path: '**', redirectTo: 'settings' }
 ];
